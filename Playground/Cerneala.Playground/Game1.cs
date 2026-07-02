030cb493faef3fa0f2a030f6e725568fe4b186f8
Avrigeanu Laurian <49100023+Chevalier12@users.noreply.github.com>
2026-07-02 20:02:38 +0300

    Use drawing abstraction in playground

--- Playground/Cerneala.Playground/Game1.cs
+++ Playground/Cerneala.Playground/Game1.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+using Cerneala.Drawing;
+using Cerneala.Drawing.MonoGame;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,6 +10,10 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private DrawCommandList _drawCommands;
+    private DrawingContext _drawing;
+    private MonoGameDrawingBackend _drawingBackend;
+    private Texture2D _whitePixel;
 
     public Game1()
     {
@@ -18,24 +24,23 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
+        _whitePixel = new Texture2D(GraphicsDevice, 1, 1);
+        _whitePixel.SetData(new[] { Color.White });
+        _drawCommands = new DrawCommandList();
+        _drawing = new DrawingContext(_drawCommands);
+        _drawingBackend = new MonoGameDrawingBackend(_spriteBatch, _whitePixel);
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
-        // TODO: Add your update logic here
 
         base.Update(gameTime);
     }
@@ -44,7 +49,13 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(global::Cerneala.GameBootstrap.CreateDefaultClearColor());
 
-        // TODO: Add your drawing code here
+        _drawCommands.Clear();
+        _drawing.FillRectangle(new DrawRect(32, 32, 240, 96), new DrawColor(20, 20, 24, 220));
+        _drawing.DrawRectangle(new DrawRect(32, 32, 240, 96), DrawColor.White, 2);
+
+        _spriteBatch.Begin();
+        _drawingBackend.Render(_drawCommands);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
